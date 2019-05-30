203e3aece818c51e5ef210ebfc19997f130361dd
vainamoinen <weixinluwx@foxmail.com>
2019-05-30 19:36:26 +0800

    big data

--- plot_distribution_watdiv.py
+++ plot_distribution_watdiv.py
@@ -56,8 +56,8 @@ for i in range(list_len):
     log_cnt_list.append(math.log(cnt_list[i]))
 
 plt.plot(log_deg_list,log_cnt_list,'.')
-plt.xlabel("degree")
-plt.ylabel("count")
+plt.xlabel("log_degree")
+plt.ylabel("log_count")
 plt.title(dataset_name+" dataset")
 plt.savefig("log_distribution_"+dataset_name+".png")
 plt.show()
