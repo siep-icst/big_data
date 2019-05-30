83c62675982df38569439114755c0ede60a3a7ee
vainamoinen <weixinluwx@foxmail.com>
2019-05-30 15:00:31 +0800

    big data

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
-dataset_name="yeast"
-data_path="yeast_2.txt"
+dataset_name="rmat100M"
+data_path="rmat100M.txt"
 
 degree_map={}
 with open(data_path,'r') as edge_file:
